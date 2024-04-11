f7360acfb893b2655925e3affc8799fed6a30ea6
Cmmitchem <c.m.mitchem@tcu.edu>
2024-04-11 13:40:51 -0500

    added get function to flask api /upload in order to retrieve file path to run PDFembeddings file with that PDF

--- backend/server.py
+++ backend/server.py
@@ -4,7 +4,7 @@ import logging
 import datetime
 import os
 
-from flask import Flask, flash, request, redirect, url_for, session
+from flask import Flask, flash, request, redirect, url_for, session, send_from_directory, jsonify
 from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
 
@@ -46,9 +46,26 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
+DOWNLOAD_DIRECTORY ='./pdfUploads/test_docs'
+# to download files from the flask API
+# to download these files directly run 'http://127.0.0.1:5000/upload/DOCUMENT NAME'
+@app.route('/upload/<path:path>', methods=['GET'])
+def get_files(path):
+    try: 
+      response = {
+          'directory': DOWNLOAD_DIRECTORY, 
+          'path': path
+      }
+      return send_from_directory(DOWNLOAD_DIRECTORY, path, as_attachment=True)
+    except FileNotFoundError:
+        print(FileNotFoundError)
+
 @app.route('/upload', methods=['GET'])
 def index():
-      return {}
+    return jsonify({'plat_list': plat_list})
+    #return {}
+# functions to upload PDFs to the testfolder on a local machine 
 
 @app.route('/upload', methods=['POST'])
 @cross_origin()
@@ -62,6 +79,8 @@ def fileUpload():
         flash('No file part')
         return redirect(request.url)
     file = request.files['file'] 
+    #file_read = file.read()
+    print(file)
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
     if file.filename == '':
@@ -70,14 +89,20 @@ def fileUpload():
         
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
+        print(filename)
         destination="/".join([target, filename])
+        print(destination)
         file.save(destination)
         session['uploadFilePath']=destination
         #return("success")
-    response=response={
-        'file': 'file', 
-        'filename': 'filename'
-    }      
+
+        #request.post()
+        file_features_dict = dict({'File Name': filename, 
+                                   'File': destination 
+                                   #'Path': destination
+                               }) 
+    plat_list.append(file_features_dict)
+   # return {'plat_list': plat_list}   
     return redirect(url_for('index'))
 	
 # Running app
